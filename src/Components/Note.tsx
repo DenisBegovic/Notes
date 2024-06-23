aa240a3716d80e8310f065bde2a70af3be3d4ec6
Denis Begovic <denni.begovic@gmail.com>
2024-06-23 21:39:51 +0200

    Made the notes refresh after deleting it

--- src/Components/Note.tsx
+++ src/Components/Note.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useState } from "react";
 import EditNoteButton from "./Edit";
+import { useRouter } from 'next/navigation';
 
 type Note = {
     id: string,
@@ -14,13 +15,18 @@ type DeleteFunction = (id: string) => void;
 
 export default function Note({ note, deleteNote }: { note: Note, deleteNote: DeleteFunction }) {
     const [mouseIsOver, setMouseIsOver] = useState(false);
-
+    const router = useRouter();
     const { id, title, content, created } = note || {};
+
+    function handleClick(id: string) {
+        deleteNote(id);
+        router.refresh();
+    }
 
     return <div className="card" onMouseOver={() => setMouseIsOver(true)} onMouseLeave={() => setMouseIsOver(false)}>
         <div className="title-area">
             <h3>{title}</h3>
-            {mouseIsOver && <button className="delete-btn" onClick={() => { deleteNote(id) }}>Delete</button>}
+            {mouseIsOver && <button className="delete-btn" onClick={() => { handleClick(id) }}>Delete</button>}
         </div>
         <hr></hr>
         <p>{content}</p>
