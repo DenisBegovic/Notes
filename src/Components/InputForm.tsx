243882dcd4fd047844ac8cbd1bf64f50627f2cf6
Denis Begovic <denni.begovic@gmail.com>
2024-06-25 17:39:46 +0200

    Fixed add note form

--- src/Components/InputForm.tsx
+++ src/Components/InputForm.tsx
@@ -1,22 +1,22 @@
 "use client";
 import { useState } from "react";
 import { useRouter } from 'next/navigation';
+import styles from "@/Components/newNote.component.module.css";
 
 export default function InputForm({ handleSubmitEvent }: any) {
     const [title, setTitle] = useState("");
     const [content, setContent] = useState("");
     const router = useRouter();
 
-    return <form className="add-note-form" onSubmit={(event) => {
+    return <form className={styles.form} onSubmit={(event) => {
         event.preventDefault();
         handleSubmitEvent(title == "" ? "Untitled" : title, content);
         router.refresh();
         setTitle("");
         setContent("");
     }}>
-        <input spellCheck="false" type="text" value={title} onChange={(event) => { setTitle(event.target.value) }} placeholder="Title" />
-
-        <textarea spellCheck="false" value={content} onChange={(event) => { setContent(event.target.value) }}></textarea>
-        <button type="submit">Create new note</button>
+        <input className={styles.title} spellCheck="false" type="text" value={title} onChange={(event) => { setTitle(event.target.value) }} placeholder="Title" />
+        <textarea className={styles.content} spellCheck="false" value={content} onChange={(event) => { setContent(event.target.value) }}></textarea>
+        <button className={styles.submitButton} type="submit">Create new note</button>
     </form>
 }
